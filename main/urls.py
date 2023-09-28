1802650779151dbb49dcf471d651149a0a62e7f2
tvadhisti <tiva.adhisti@ui.ac.id>
2023-09-28 21:36:53 +0700

    completed tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register, login_user, logout_user
+from main.views import register, login_user, logout_user, edit_product, delete_product
 
 app_name = 'main'
 
@@ -15,4 +15,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),  # adjust the function name
 ]
--- main/views.py
+++ main/views.py
@@ -74,6 +74,30 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 
+def edit_product(request, id):
+    # Get product by ID
+    product = Product.objects.get(pk = id)
+
+    # Set product as instance of form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Save the form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data by ID
+    product = Product.objects.get(pk=id)
+    # Delete data
+    product.delete()
+    # Return to the main page
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+
 # return data in XML format
 # This function accepts a request as the parameter.
 # Create a variable to store all fetched Product objects.
