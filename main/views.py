ad00004b4641309122c108c864fda15b28f886da
tvadhisti <tiva.adhisti@ui.ac.id>
2023-10-05 11:04:01 +0700

    completed tutorial 5

--- main/views.py
+++ main/views.py
@@ -11,6 +11,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login,  logout
 from django.contrib.auth.decorators import login_required
 import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login')
 
@@ -125,3 +126,22 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
